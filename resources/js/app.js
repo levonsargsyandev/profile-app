a16502b22424c236b213621f7af0808675ec69ae
Levon <levonsargsyandev@gmail.com>
2022-01-26 12:51:27 +0400

    install and configure the project

---
+++ resources/js/app.js
@@ -0,0 +1,19 @@
+require('./bootstrap');
+window.Vue = require('vue').default;
+
+import Vue from 'vue'
+import router from "./router";
+import Layout from "./Layout";
+
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+new Vue({
+    router,
+    render: h => h(Layout)
+}).$mount('#app');
