9c4cc262fb23468ede62d68d598b4a42e5789376
Levon <levonsargsyandev@gmail.com>
2022-03-03 15:03:14 +0400

    first commit

---
+++ resources/js/pages/ComponentForGitTests.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>First</div>
+    <div>Second</div>
+    <div>Third</div>
+    <div>Fifth</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
